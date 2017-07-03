b503d02c9aaad3ab9c50f399f08c24e81aa93b07
2 b <isaquebc@gmail.com>
2017-07-02 23:31:28 -0300

    code review

--- novos craques/src/atoms/lib/InputText.js
+++ novos craques/src/atoms/lib/InputText.js
@@ -4,17 +4,19 @@ import PropTypes from "prop-types";
 export default class InputText extends Component{
 
 	render(){
-
 		const{ className, id, onChange, value }= this.props;
-
 		return(
-				// os ${}  concatena string,  entre as chaves é string 
-				// antes do cifrão é variavel NÃO ESQUECER ASPAS INVERTIDA
-				// o className do button é uma variável pois no nome pode ser alterado
-					<input className={`input-text ${className}`} id={ id } onChange={ onChange } value={ value } type="text" />
-
-			);
-
+			// os ${}  concatena string,  entre as chaves é string 
+			// antes do cifrão é variavel NÃO ESQUECER ASPAS INVERTIDA
+			// o className do button é uma variável pois no nome pode ser alterado
+			<input
+				className={`input-text ${className}`} 
+				id={ id } 
+				onChange={ onChange } 
+				value={ value } 
+				type="text" 
+			/>
+		);
 	}
 }
 
