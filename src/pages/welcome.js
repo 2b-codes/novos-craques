d0aedab65b8ac0cde3d6e83b5985235cdefd5fac
isaquebc <isaquebc@gmail.com>
2017-07-23 20:21:46 -0300

    [master]

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import { ButtonIcon } from "../molecules";
-import { InputDate } from "../atoms";
+import { DropDown } from "../atoms";
 
 
 export default  class Welcome extends Component{ 
@@ -12,27 +12,46 @@ export default  class Welcome extends Component{
 		this.state={
 			values: {
 				number: "",
-				cpf: ""
-			}
+				cpf: "",
+				select: ""
+			},
+			options: [
+				{id: "x", label:"1"},
+				{id: "w", label:"2"},
+				{id: "e", label:"3"},
+				{id: "r", label:"4"},
+				{id: "g", label:"5"},
+				{id: "a", label:"6"},
+				{id: "d", label:"7"},
+				{id: "aSA", label:"7"},
+				{id: "dASD", label:"7"},
+				{id: "dASSDFD", label:"7"},
+				{id: "dDSFG", label:"7"},
+				{id: "dZXC", label:"7"},
+				{id: "TRYd", label:"7"},
+				{id: "dGDF", label:"7"},
+				{id: "Cd", label:"7"},
+				{id: "dDHTR", label:"7"},
+				{id: "WEd", label:"7"},
+				{id: "GJHd", label:"7"},
+				{id: "m", label:"8"},
+			]
 		};
 	}
 
 	handleChange(e) {
 		const {values} = this.state;
 		const {id, value} = e.target;
+		console.log([e.target]);
 		values[id] = value;
 		this.setState({values});
 	}
 
-	render(){
+	render() {
+		console.log("STATE", this.state);
 		return(
-		   <div >
-			   <InputDate 
-				   onChange={this.handleChange} 
-				   value={this.state.values["date"]}
-				   noInput={false}
-				   noTime
-				 />
+		   <div>
+		   		WELCOME
 			</div>
 		);
 	}
