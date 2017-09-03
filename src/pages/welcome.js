413a93b8e4a44a6d25e376217598d2183df32c09
Isaque Costa <isaquebc@gmail.com>
2017-09-03 15:38:17 -0300

    [master]

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -7,51 +7,10 @@ import { DropDown } from "../atoms";
 
 export default  class Welcome extends Component{ 
 
-	constructor(props){ 	
-		super(props);
-		this.state={
-			values: {
-				number: "",
-				cpf: "",
-				select: ""
-			},
-			options: [
-				{id: "x", label:"1"},
-				{id: "w", label:"2"},
-				{id: "e", label:"3"},
-				{id: "r", label:"4"},
-				{id: "g", label:"5"},
-				{id: "a", label:"6"},
-				{id: "d", label:"7"},
-				{id: "aSA", label:"7"},
-				{id: "dASD", label:"7"},
-				{id: "dASSDFD", label:"7"},
-				{id: "dDSFG", label:"7"},
-				{id: "dZXC", label:"7"},
-				{id: "TRYd", label:"7"},
-				{id: "dGDF", label:"7"},
-				{id: "Cd", label:"7"},
-				{id: "dDHTR", label:"7"},
-				{id: "WEd", label:"7"},
-				{id: "GJHd", label:"7"},
-				{id: "m", label:"8"},
-			]
-		};
-	}
-
-	handleChange(e) {
-		const {values} = this.state;
-		const {id, value} = e.target;
-		console.log([e.target]);
-		values[id] = value;
-		this.setState({values});
-	}
-
 	render() {
-		console.log("STATE", this.state);
 		return(
-		   <div>
-		   		WELCOME
+			<div>
+				TESTE
 			</div>
 		);
 	}
