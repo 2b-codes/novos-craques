73575bbe3539d9c047cd9210e49107b6a7917da3
Isaque Costa <isaquebc@gmail.com>
2017-09-09 12:56:33 -0300

    [OR003]

---
+++ src/organisms/index.js
@@ -0,0 +1,5 @@
+import Coursel from "./lib/Coursel";
+
+export {
+	Coursel
+};
---
+++ src/organisms/lib/Coursel.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Icon } from "../../atoms";
+
+export default class Coursel extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			step: 0,
+			steps: props.children.length-1
+		};
+	}
+
+	renderChildren() {
+		return this.props.children[this.state.step];
+	}
+
+	navigateAction(n) {
+		if(this.props.infinit){
+			if(n>this.state.steps){this.setState({step: 0}); return;}
+			else if(n<0){this.setState({step: this.state.steps}); return;}
+			this.setState({step: n});
+			return;
+		}		
+		if(n>this.state.steps) return;
+		else if(n<0) return;
+		this.setState({step: n});
+	}
+
+	render(){
+		const{ className }=this.props;
+		return(
+			<div className={`coursel ${className}`}>
+				<Icon 
+					type="downArrow" 
+					className="prev" 
+					clickAction={()=>this.navigateAction(this.state.step -1)}
+					id="courselPrevAction"
+				/>
+				<div className="children">
+					{this.renderChildren()}
+				</div>
+				<Icon 
+					type="downArrow" 
+					className="next" 
+					clickAction={()=>this.navigateAction(this.state.step +1)}
+					id="courselNextAction"
+				/>
+			</div>
+		);
+	}
+}
+
+Coursel.PropTypes={
+
+	src: PropTypes.string,
+	onClick: PropTypes.func,
+	className: PropTypes.string.isRequired,
+	children : PropTypes.string.isRequired,
+	id: PropTypes.string.isRequired,
+	value: PropTypes.string.isRequired,
+	text:  PropTypes.string.isRequired,
+	placeholder: PropTypes.string.isRequired,
+};
