9362252afbeb6f4e1640dbad20827ac16f2cc392
Isaque Costa <isaquebc@gmail.com>
2017-09-16 17:23:52 -0300

    [T003]

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { SubHeader } from "../organisms";
 import { MenuProfile } from "../organisms";
 import { LoginForm } from "../organisms";
-import { MenuOptionLogOut } from "../organisms";
+import { WrapperModal } from "../molecules";
 import { Informations } from "../templates";
 import { cardDepoimentModel, cardMotivationModel, aboutUsModel } from "../models";
 
@@ -11,11 +11,17 @@ export default  class Welcome extends Component{
 	render(){
 		return(
 			<div>
+				<WrapperModal 
+					title="titulo"
+					children="SAlve"
+					visible={true}
+					closeAction={() => ""}
+				/>			
 				<Informations 
 					depoiments={cardDepoimentModel}
 					motivations={cardMotivationModel}
-					aboutUs={aboutUsModel} 
-				/>			
+					aboutUs={aboutUsModel}
+				/>
 			</div>
 		);
 
