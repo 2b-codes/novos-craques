4765139b4d77cec2c1e16b50ccb85a73db3b779c
Isaque Costa <isaquebc@gmail.com>
2017-09-28 21:02:06 -0300

    [master]

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -9,16 +9,38 @@ import { Informations } from "../templates";
 import { cardDepoimentModel, cardMotivationModel, aboutUsModel } from "../models";
 
 export default  class Welcome extends Component{ 
+
+	constructor(props){
+		super(props);
+		this.state = {
+			fullscreen: false,
+		};
+	}
+
+	renderSize() {
+		const width = window.screen.width;
+		const height = window.screen.height;
+		this.setState({fullscreen: !this.state.fullscreen});
+	}
+
+
+	changeColor() {
+		document.querySelector("#ytplayer").style.borderColor = "#CCC";
+	}
 	
 	render(){
 		return(
-			<div>
-				<HomeTemplate />
-				<Informations 
-					depoiments={cardDepoimentModel}
-					motivations={cardMotivationModel}
-					aboutUs={aboutUsModel}
-				/>
+			<div className={`aqui ${this.state.fullscreen ? "--fullscreen" : ""}`}>
+				<div className={`iVideo ${this.state.fullscreen ? "--fullscreen" : ""}`}>
+					<iframe
+						className={`SALVE ${this.state.fullscreen ? "--fullscreen" : ""}`}
+						id="ytplayer"
+						type="text/html"
+						src="https://www.youtube.com/embed/QzRIPJMK6l8?cc_load_policy=1&controls=0&enablejsapi=1&loop=1&modestbranding=1&playsinline=1&rel=0&iv_load_policy=3"
+						frameBorder="0"
+					/>
+					<button className="button" onClick={e => {e.stopPropagation(); this.renderSize();}}>Mudar tabanho da tela</button>
+				</div>
 			</div>
 		);
 
